862a3a8a5d58d508ed7621a9d84f3ae5759d4c91
Joe Lathrop <jhlathrop24@gmail.com>
2024-08-08 20:36:59 -0600

    update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
     res.render('home');
